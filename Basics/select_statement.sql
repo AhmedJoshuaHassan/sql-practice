7be6b1eae86111960b9ddbac702f90b687244c47
AhmedJoshuaHassan <hassanahmedjoshua@gmail.com>
2026-01-31 07:01:18 +0100

    Write Select query

--- Basics/select_statement.sql
+++ Basics/select_statement.sql
@@ -1,5 +1,30 @@
-SELECT * 
+/* 
+'SELECT *' Selects all the columns on the table 
+FROM specifies the database and table to quary
+';' This specifies the end of the query
+ */
+
+SELECT *
 FROM parks_and_recreation.employee_demographics;
 
-SELECT * 
-FROM employee_salary;
+SELECT *
+FROM parks_and_recreation.employee_salary;
+
+/* 
+Specific columns can be selected
+Write specific column name in the SELECT 
+*/
+
+SELECT first_name,
+last_name,
+birth_name,
+age,
+age+10
+FROM parks_and_recreation.employee_demographics;
+
+/* DISTINCT
+This selects the unique values
+*/
+
+SELECT DISTINCT gender
+FROM parks_and_recreation.employee_demographics;
